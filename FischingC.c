29153304178f66d7af342fc20b77a80b1e17f41c
Tealtie <125929994+Tealtie@users.noreply.github.com>
2025-02-19 18:37:28 +0000

    Update FischingC.c

--- FischingC.c
+++ FischingC.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-printf("Welcome to FischingC!");
+printf("Welcome to FischingC!\n");
+printf("The aim of this game is to catch every fish in the world!\n");
 };
