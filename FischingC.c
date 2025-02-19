0ffcf581eabaad6c2c15645c525402969c2a6bbe
Tealtie <125929994+Tealtie@users.noreply.github.com>
2025-02-19 18:54:49 +0000

    Update FischingC.c

--- FischingC.c
+++ FischingC.c
@@ -2,5 +2,6 @@
 #include <stdlib.h>
 int main() {
 printf("Welcome to FischingC!\n");
-printf("The aim of this game is to catch every fish in the world!\n");
+printf("The aim of this game is to catch every fish in the C!\n");
+printf("But to do that, you need a fisching rod)
 };
