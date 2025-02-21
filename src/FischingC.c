259c9565718b47e361ee98aa8deacdda4a933c6e
Tealtie <125929994+Tealtie@users.noreply.github.com>
2025-02-21 20:20:35 +0000

    Enhance FischingC welcome message with shop interaction for buying a fishing rod

--- src/FischingC.c
+++ src/FischingC.c
@@ -4,4 +4,17 @@ int main() {
 printf("Welcome to FischingC!\n");
 printf("The aim of this game is to catch every fish in the C!\n");
 printf("But to do that, you need a fisching rod.\n");
+printf("You can buy one in the shop.\n");
+printf("Since you are a beginner, you can only afford the cheapest one.\n");
+printf("You can buy 'Wooden Fisching Rod' for 10 Cents.\n");
+printf("Do you want to buy it? (y/n)\n");
+char answer;
+scanf("%c", &answer);
+if (answer == 'y') {
+printf("YOU BOUGHT 'WOODEN FISCHING ROD'!\n");
+printf("Now you can start fisching!\n");
+};
+if (answer == 'n') {
+printf("You didn't buy 'Wooden Fisching Rod'!\n");
+};
 };
