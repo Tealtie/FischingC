da148ea9f8e6cc6da5ca400ae505dd1c1ff2da49
Tealtie <125929994+Tealtie@users.noreply.github.com>
2025-02-21 19:58:56 +0000

    Add FischingC binary and fix welcome message punctuation

--- src/FischingC.c
+++ src/FischingC.c
@@ -3,5 +3,5 @@
 int main() {
 printf("Welcome to FischingC!\n");
 printf("The aim of this game is to catch every fish in the C!\n");
-printf("But to do that, you need a fisching rod");
+printf("But to do that, you need a fisching rod.\n");
 };
